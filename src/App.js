2853fb7d01efc2bfcc4ced4e16495e09a4d9ad55
unknown <millagoss19@gmail.com>
2022-09-15 10:53:22 +0300

    first commit(project added)

---
+++ src/App.js
@@ -0,0 +1,131 @@
+import React, { useRef, useState, useEffect } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import Photo from './Photo';
+
+const clientId = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
+
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
+
+function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [newImages, setNewImages] = useState(false);
+
+  const checkInitValue = useRef(true);
+
+  const fetchPhotos = async () => {
+    setIsLoading(true);
+    let url;
+    const urlPage = `&page=${page}`;
+
+    if (searchTerm) {
+      url = `${searchUrl}${clientId}${urlPage}&query=${searchTerm}`;
+    } else {
+      url = `${mainUrl}${clientId}${urlPage}`;
+    }
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      setPhotos((oldPhotos) => {
+        if (searchTerm && page === 1) {
+          return data.results;
+        } else if (searchTerm) {
+          return [...oldPhotos, ...data.results];
+        } else {
+          return [...oldPhotos, ...data];
+        }
+      });
+      setNewImages(false);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setNewImages(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPhotos();
+  }, [page]);
+
+  useEffect(() => {
+    if (!checkInitValue.current) {
+      checkInitValue.current = true;
+      return;
+    }
+    if (!newImages) return;
+    if (isLoading) return;
+
+    setPage((oldPage) => oldPage + 1);
+    console.log('walla');
+  }, [newImages]);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  const handleScroll = () => {
+    if (window.scrollY + window.innerHeight >= document.body.scrollHeight - 2) {
+      setNewImages(true);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!searchTerm) return;
+    if (searchTerm && page === 1) {
+      fetchPhotos();
+      return;
+    }
+    setPage(1);
+  };
+
+  return (
+    <main>
+      <section className='search'>
+        <form className='search-form'>
+          <input
+            type='text'
+            placeholder='search'
+            className='form-input'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button type='submit' className='submit-btn' onClick={handleSubmit}>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className='photos'>
+        <div className='photos-center'>
+          {photos.map((photo) => {
+            return <Photo key={photo.id} photo={photo} />;
+          })}
+        </div>
+        {isLoading && <h2 className='loading'>loading...</h2>}
+      </section>
+    </main>
+  );
+}
+
+export default App;
+
+//  useEffect(() => {
+//    const event = window.addEventListener('scroll', () => {
+//      if (
+//        !isLoading &&
+//        window.scrollY + window.innerHeight >= document.body.scrollHeight - 2
+//      ) {
+//        setPage((oldPage) => oldPage + 1);
+//        console.log('ola');
+//      }
+//    });
+
+//    return () => window.removeEventListener('scroll', event);
+//  }, []);
---
+++ src/Photo.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const Photo = ({ photo }) => {
+  // console.log(photo);
+  const {
+    urls: { regular, full },
+    alt_description,
+    likes,
+    user: {
+      first_name,
+      portfolio_url,
+      profile_image: { medium },
+    },
+  } = photo;
+  console.log(photo);
+
+  const newPhoto = {
+    url: regular,
+    fullImageUrl: full,
+    noOfLikes: likes,
+    userName: first_name,
+    portfolio: portfolio_url,
+    image: medium,
+    alt_desc: alt_description || 'no_description',
+  };
+
+  const { url, noOfLikes, userName, portfolio, image, alt_desc, fullImageUrl } =
+    newPhoto;
+  // console.log(portfolio);
+  return (
+    <article className='photo'>
+      <a href={fullImageUrl} target='_blank'>
+        <img src={url} alt={alt_desc} />
+      </a>
+      <div className='photo-info'>
+        <div>
+          <h4>{userName}</h4>
+          <p>{noOfLikes} likes</p>
+        </div>
+        <a href={portfolio} target='_blank'>
+          <img src={image} alt={userName} className='user-img' />
+        </a>
+      </div>
+    </article>
+  );
+};
+
+export default Photo;
